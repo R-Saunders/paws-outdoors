2827b31374958a7e2accf34ed9f950bcb288cf19
Rich Saunders <richsaunders@protonmail.com>
2023-09-24 21:16:36 +0100

    add google fonts, create header, start work on homepage hero and header

---
+++ src/components/header.tsx
@@ -0,0 +1,104 @@
+"use client";
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Caveat, Comfortaa } from "next/font/google";
+
+const caveat = Caveat({ subsets: ["latin"] });
+const comfortaa = Comfortaa({ subsets: ["latin"] });
+
+const navigation = [
+	{ name: "Services", href: "/services" },
+	{ name: "About Us", href: "/about-us" },
+	{ name: "Book Now", href: "/book" },
+];
+
+export default function Example() {
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+	return (
+		<header className="absolute z-10 min-w-full text-lime-900">
+			<nav
+				className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8"
+				aria-label="Global"
+			>
+				<div className="flex lg:flex-1">
+					<a
+						href="#"
+						className="-m-1.5 p-1.5 flex items-center justify-center gap-4"
+					>
+						<img className="h-16 w-auto" src="logo.svg" alt="" />
+						<span className={`${caveat.className} text-5xl`}>
+							Paws Outdoors
+						</span>
+					</a>
+				</div>
+				<div className="flex lg:hidden">
+					<button
+						type="button"
+						className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 pt-4 text-lime-800 z-10"
+						onClick={() => setMobileMenuOpen(true)}
+					>
+						<span className="sr-only">Open main menu</span>
+						<Bars3Icon className="h-6 w-6" aria-hidden="true" />
+					</button>
+				</div>
+				<div className={`${comfortaa.className} hidden lg:flex lg:gap-x-12`}>
+					{navigation.map((item) => (
+						<a
+							key={item.name}
+							href={item.href}
+							className="text-sm font-semibold leading-6 text-lime-900 px-2 py-1 hover:text-lime-700 last:bg-lime-900 rounded last:text-amber-50 last:hover:bg-lime-800 last:hover:text-amber-50"
+						>
+							{item.name}
+						</a>
+					))}
+				</div>
+			</nav>
+			<Dialog
+				as="div"
+				className="lg:hidden"
+				open={mobileMenuOpen}
+				onClose={setMobileMenuOpen}
+			>
+				<div className="fixed inset-0 z-20" />
+				<Dialog.Panel className="fixed inset-y-0 right-0 z-20 w-full overflow-y-auto bg-amber-50  px-4 py-4 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+					<div className="flex items-center justify-between">
+						<a
+							href="#"
+							className="-m-1.5 p-1.5 flex items-center justify-center gap-4"
+						>
+							<img className="h-16 w-auto" src="logo.svg" alt="" />
+							<span className={`${caveat.className} text-5xl text-lime-900`}>
+								Paws Outdoors
+							</span>
+						</a>
+						<button
+							type="button"
+							className="-m-2.5 rounded-md p-2.5 pt-4 text-lime-900 z-20"
+							onClick={() => setMobileMenuOpen(false)}
+						>
+							<span className="sr-only">Close menu</span>
+							<XMarkIcon className="h-6 w-6" aria-hidden="true" />
+						</button>
+					</div>
+					<div className="mt-6 flow-root">
+						<div className="-my-6 divide-y divide-lime-900/10">
+							<div className="space-y-2 py-6">
+								{navigation.map((item) => (
+									<a
+										key={item.name}
+										href={item.href}
+										className={`${comfortaa.className} -mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-lime-900 hover:bg-amber-100`}
+									>
+										{item.name}
+									</a>
+								))}
+							</div>
+						</div>
+					</div>
+				</Dialog.Panel>
+			</Dialog>
+		</header>
+	);
+}
---
+++ src/components/hero-home.tsx
@@ -0,0 +1,45 @@
+import { Comfortaa } from "next/font/google";
+
+const comfortaa = Comfortaa({ subsets: ["latin"] });
+
+export default function HeroHome() {
+	return (
+		<section className={`${comfortaa.className} relative bg-[url(/nova-wall-lake-district.jpg)] bg-cover bg-center bg-no-repeat`}>
+			<div className="absolute inset-0 bg-amber-50/60 sm:bg-transparent sm:from-amber-50/75 sm:to-amber-50/25 sm:bg-gradient-to-r"></div>
+
+			<div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+				<div className="max-w-xl text-center sm:text-left">
+					<h1 className="text-3xl font-extrabold sm:text-5xl text-lime-900">
+						Pet care
+						<strong className="block font-extrabold text-lime-900">
+							You can trust.
+						</strong>
+					</h1>
+
+					<p className="mt-4 max-w-lg sm:text-xl/relaxed">
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+						illo tenetur fuga ducimus numquam ea!
+					</p>
+
+					<div className="mt-8 flex flex-wrap gap-4 text-center">
+						<a
+							href="#"
+							className="block w-full rounded bg-lime-900 px-12 py-3 text-sm font-medium text-amber-50 shadow hover:bg-lime-800 focus:outline-none focus:ring active:bg-lime-700 sm:w-auto"
+						>
+							Get In Touch
+						</a>
+
+						<a
+							href="#"
+							className="block w-full rounded bg-amber-50 px-12 py-3 text-sm font-medium text-lime-900 shadow hover:text-lime-700 focus:outline-none focus:ring active:text-lime-700 sm:w-auto"
+						>
+							Find Out More
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+// bg-[url('/nova-wall-lake-district.jpg')]
