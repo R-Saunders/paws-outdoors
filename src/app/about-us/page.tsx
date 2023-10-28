21ecf936bf314a94b6da7ea39d3d0022ac3c0abf
Rich Saunders <richsaunders@protonmail.com>
2023-10-28 22:48:27 +0100

    add cta to page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,11 +1,13 @@
 import HeroAbout from "@/components/hero-about";
 import Values from "@/components/values";
+import SimpleCTA from "@/components/simple-cta";
 
 export default function Contact() {
 	return (
 		<main className="pt-24">
 			<HeroAbout />
 			<Values />
+			<SimpleCTA />
 		</main>
 	);
 }
