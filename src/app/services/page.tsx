d9a7606a446990242504b5fd57dcfe51df0d5e97
Rich Saunders <richsaunders@protonmail.com>
2023-10-28 18:16:01 +0100

    add services components

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,9 +1,15 @@
 import HeroServices from "@/components/hero-services";
+import Walking from "@/components/walks";
+import Visits from "@/components/visits";
+import PuppyPlay from "@/components/puppy-play";
 
 export default function Services() {
 	return (
 		<main className="w-full">
-			<HeroServices/>
+			<HeroServices />
+			<Walking />
+			<Visits />
+			<PuppyPlay />
 		</main>
 	);
 }
