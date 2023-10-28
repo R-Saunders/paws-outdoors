fbbb3c0f29244e75b663cb0d866dfb158b3275a5
Rich Saunders <richsaunders@protonmail.com>
2023-10-28 18:16:33 +0100

    update header colors

--- src/components/hero-services.tsx
+++ src/components/hero-services.tsx
@@ -17,7 +17,7 @@ export default function HeroServices() {
 				/>
 			</div>
 			<div className="px-8 py-4 flex flex-col gap-4 lg:gap-6 l:gap-8 justify-center md:w-2/4 items-center xl:max-w-3xl">
-				<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl max-w-xl">
+				<h1 className="text-4xl font-bold tracking-tight text-lime-900 sm:text-6xl max-w-xl">
 					Your Pet&apos;s Second Best Friend
 				</h1>
 				<p className="text-xl leading-8 text-gray-600 lg:text-2xl max-w-xl">
