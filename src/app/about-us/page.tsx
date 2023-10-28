01b6a541694a064363f8c17a1e0b8b27d3146690
Rich Saunders <richsaunders@protonmail.com>
2023-10-28 21:53:09 +0100

    import new components

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,3 +1,11 @@
+import HeroAbout from "@/components/hero-about";
+import Values from "@/components/values";
+
 export default function Contact() {
-	return <main className="pt-24">About Us</main>;
+	return (
+		<main className="pt-24">
+			<HeroAbout />
+			<Values />
+		</main>
+	);
 }
